950950bfb42655a6cf6a4fac5d0c499b053efa86
yusuf-inci <yusufinci@protonmail.com>
2023-03-24 11:59:41 +0300

    updated first-instance.tf

--- ec2/first-instance.tf
+++ ec2/first-instance.tf
@@ -1,17 +1,18 @@
 provider "aws" {
-    region = us-east-2
-    #access_key = "" give credentials through awscli
-    #secret_key = ""
+  region = "us-east-2"
+  # access_key = "" give credentials through awscli
+  # secret_key = ""
 
 }
 
 resource "aws_instance" "intro" {
-    ami = "ami-0533def491c57d991"
-    instance_type = "t2.micro"
-    availability_zone = "us-east-2a"
-    key_name = baytera-key
-    vpc_security_group_ids = [ "sg-0a5f5e49e1215fa97" ]
-    tags = {
-      Name = "Baytera-Instance"
-    }
+  ami                    = "ami-0533def491c57d991"
+  instance_type          = "t2.micro"
+  availability_zone      = "us-east-2a"
+  key_name               = "baytera-key"
+  vpc_security_group_ids = ["sg-0a5f5e49e1215fa97"]
+  tags = {
+    Name    = "Baytera-Instance"
+    Project = "Baytera"
+  }
 }
