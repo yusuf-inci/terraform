a1d5e8d50cfb754f6ece8700215605997419f959
yusuf-inci <yusufinci@protonmail.com>
2023-03-24 17:01:55 +0300

    added instance.tf

---
+++ ec2-with-var/instance.tf
@@ -0,0 +1,12 @@
+resource "aws_instance" "baytera-inst" {
+    ami = var.AMIS[var.REGION]
+    instance_type = "t2.micro"
+    availability_zone = var.ZONE1
+    key_name               = "baytera-key"
+vpc_security_group_ids = ["sg-0a5f5e49e1215fa97"]
+tags = {
+  Name    = "Baytera-Instance"
+  Project = "Baytera"
+}
+  
+}
