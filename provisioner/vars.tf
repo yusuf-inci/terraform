8d3dbf0e93eca6572083bf632d4f5a1d4b9f9e8f
yusuf-inci <yusufinci@protonmail.com>
2023-03-25 12:54:11 +0300

    added vars.tf

---
+++ provisioner/vars.tf
@@ -0,0 +1,15 @@
+variable "REGION" {
+  default = "us-east-2"
+}
+
+variable "ZONE1" {
+  default = "us-east-2a"
+}
+
+variable "AMIS" {
+  type = map
+  default = {
+    us-east-2 = "ami-0533def491c57d991"
+    us-east-1 = "ami-04581fbf744a7d11f"
+  }
+}
