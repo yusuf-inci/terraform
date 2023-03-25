848d5dc5380f5a099353d5cf1fd41016bd17061a
yusuf-inci <yusufinci@protonmail.com>
2023-03-25 14:49:53 +0300

    fixed fmt issue

--- provisioner/instance.tf
+++ provisioner/instance.tf
@@ -1,35 +1,35 @@
 resource "aws_key_pair" "baytera-key" {
-    key_name = "baytera"
-    public_key = file("baytera.pub")
+  key_name   = "baytera"
+  public_key = file("baytera.pub")
 }
 
 resource "aws_instance" "baytera-inst" {
-    ami = var.AMIS[var.REGION]
-    instance_type = "t2.micro"
-    availability_zone = var.ZONE1
-    key_name = aws_key_pair.baytera-key.key_name
-    vpc_security_group_ids = ["sg-0a5f5e49e1215fa97"]
-    tags = {
-        Name    = "Baytera-Instance"
-        Project = "Baytera"
-    }
+  ami                    = var.AMIS[var.REGION]
+  instance_type          = "t2.micro"
+  availability_zone      = var.ZONE1
+  key_name               = aws_key_pair.baytera-key.key_name
+  vpc_security_group_ids = ["sg-0a5f5e49e1215fa97"]
+  tags = {
+    Name    = "Baytera-Instance"
+    Project = "Baytera"
+  }
 
-    provisioner "file" {
-        source = "web.sh"
-        destination = "/tmp/web.sh"
-    }
+  provisioner "file" {
+    source      = "web.sh"
+    destination = "/tmp/web.sh"
+  }
 
-    provisioner "remote-exec" {
-        inline = [
-            "chmod u+x /tmp/web.sh",
-            "sudo /tmp/web.sh"
-        ]
-    }
+  provisioner "remote-exec" {
+    inline = [
+      "chmod u+x /tmp/web.sh",
+      "sudo /tmp/web.sh"
+    ]
+  }
 
-    connection {
-        user = var.USER
-        private_key = file("baytera")
-        host = self.public_ip 
-      
-    }
+  connection {
+    user        = var.USER
+    private_key = file("baytera")
+    host        = self.public_ip
+
+  }
 }
