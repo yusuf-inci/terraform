99849b674c9b2bbfbf2e23217403f0f135ed9cab
yusuf-inci <yusufinci@protonmail.com>
2023-03-27 12:42:39 +0300

    removed keys

---
+++ sample-flask/main.tf
@@ -0,0 +1,70 @@
+provider "aws" {
+  region = "us-west-2"
+}
+
+resource "aws_key_pair" "baytera-key" {
+  key_name   = "baytera-key"
+  public_key = file("~/.ssh/baytera-key.pub")
+}
+
+resource "aws_security_group" "baytera-sg" {
+  name_prefix = "baytera-sg"
+
+  ingress {
+    from_port = 22
+    to_port   = 22
+    protocol  = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 80
+    to_port   = 80
+    protocol  = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port = 443
+    to_port   = 443
+    protocol  = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+}
+
+resource "aws_instance" "baytera-inst" {
+  ami           = "ami-0c55b159cbfafe1f0"
+  instance_type = "t2.micro"
+  key_name      = aws_key_pair.baytera-key.key_name
+  security_groups = [aws_security_group.baytera-sg.name]
+
+  connection {
+    type        = "ssh"
+    user        = "ec2-user"
+    private_key = file("~/.ssh/baytera-key")
+    host        = self.public_ip
+  }
+
+  provisioner "file" {
+    source      = "./app.py"
+    destination = "/home/ec2-user/app.py"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "sudo yum -y update",
+      "sudo amazon-linux-extras install -y python3.8",
+      "sudo yum -y install python3-pip",
+      "sudo python3 -m pip install flask",
+      "sudo python3 /home/ec2-user/app.py &"
+    ]
+  }
+
+  tags = {
+    Name = "baytera-instance"
+  }
+}
+
+output "public_ip" {
+  value = aws_instance.baytera-inst.public_ip
+}
