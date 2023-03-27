fb544e059c19a0bb0812c9c53762bab1cc48b547
yusuf-inci <yusufinci@protonmail.com>
2023-03-27 10:48:17 +0300

    update AMIS

--- provisioner/vars.tf
+++ provisioner/vars.tf
@@ -6,8 +6,8 @@ variable "ZONE1" {
   default = "us-east-2a"
 }
 
-variable "AMIS" {
-  type = map(any)
+variable AMIS {
+  type = map
   default = {
     us-east-2 = "ami-0533def491c57d991"
     us-east-1 = "ami-04581fbf744a7d11f"
