d959fc4669f6b59ba6385ac36a1caaca8dec0d95
yusuf-inci <yusufinci@protonmail.com>
2023-03-27 13:20:27 +0300

    updated cidr block

--- sample-flask/main.tf
+++ sample-flask/main.tf
@@ -14,21 +14,21 @@ resource "aws_security_group" "baytera-sg" {
     from_port = 22
     to_port   = 22
     protocol  = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["your_ip_address/32"]
   }
 
   ingress {
     from_port = 80
     to_port   = 80
     protocol  = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["your_ip_address/32"]
   }
 
   ingress {
     from_port = 443
     to_port   = 443
     protocol  = "tcp"
-    cidr_blocks = ["0.0.0.0/0"]
+    cidr_blocks = ["your_ip_address/32"]
   }
 }
 
