a482af8fc70ce4b5d4df61a9248eb91775e9dae7
yusuf-inci <yusufinci@protonmail.com>
2023-03-27 23:31:46 +0300

    added provider.tf and vars.tf

---
+++ multi-resource/vars.tf
@@ -0,0 +1,27 @@
+variable "REGION" {
+  default = "us-east-2"
+}
+
+variable "ZONE1" {
+  default = "us-east-2a"
+}
+
+variable "AMIS" {
+  type = map(any)
+  default = {
+    us-east-2 = "ami-0533def491c57d991"
+    us-east-1 = "ami-04581fbf744a7d11f"
+  }
+}
+
+variable "USER" {
+  default = "ec2-user"
+}
+
+variable "PUB_KEY" {
+    default = "baykey.pub"
+}
+
+variable "PRIV_KEY" {
+    default = "baykey"
+}
